a60c1df8bdc3467bfcf5c442e1f130cd2802c33c
field <field@ccom.unh.edu>
2018-04-25 09:52:50 -0400

    Fixed crash when no data was returned.

--- nodes/br24_radar_node.py
+++ nodes/br24_radar_node.py
@@ -13,6 +13,8 @@ def radar_listener():
     rospy.init_node('br24_radar')
     
     radar = br24_radar.br24_radar.Radar()
+    radar.turnOn()
+    radar.setRange(2500)
 
     timestamp = datetime.datetime.utcfromtimestamp(rospy.Time.now().to_time()).isoformat()
     bag = rosbag.Bag('nodes/br24_radar_'+('-'.join(timestamp.split(':')))+'.bag', 'w')
@@ -23,16 +25,18 @@ def radar_listener():
         except socket.error:
             break
 
-        sector = RadarSector()
+        if radar_data is not None:
 
-        for sl in radar_data['scanlines']:
-            scanline = RadarScanline()
-            scanline.angle = sl['angle']
-            scanline.range = sl['range']
-            scanline.intensities = sl['intensities']
-            sector.scanlines.append(scanline)
-        pub.publish(sector)
-        bag.write('/radar',sector)
+            sector = RadarSector()
+
+            for sl in radar_data['scanlines']:
+                scanline = RadarScanline()
+                scanline.angle = sl['angle']
+                scanline.range = sl['range']
+                scanline.intensities = sl['intensities']
+                sector.scanlines.append(scanline)
+            pub.publish(sector)
+            bag.write('/radar',sector)
     bag.close()
 
 if __name__ == '__main__':
