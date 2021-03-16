915385010894ec817bf9d1a9f720fd1cc5a5f082
Roland Arsenault <roland@ccom.unh.edu>
2021-03-16 14:40:51 -0400

    Change topic to relative path and read frame_id from params

--- nodes/br24_radar_node.py
+++ nodes/br24_radar_node.py
@@ -4,41 +4,37 @@ import rospy
 from marine_msgs.msg import RadarSectorStamped
 from marine_msgs.msg import RadarScanline
 import br24_radar.br24_radar
-import datetime
 
 
 def radar_listener():
-    pub = rospy.Publisher('/radar',RadarSectorStamped,queue_size=10)
+    pub = rospy.Publisher('radar',RadarSectorStamped,queue_size=10)
     rospy.init_node('br24_radar')
     
     radar = br24_radar.br24_radar.Radar()
-    #radar.turnOn()
-    #radar.setRange(2500)
-
-    timestamp = datetime.datetime.utcfromtimestamp(rospy.Time.now().to_time()).isoformat()
+    
+    frame_id = rospy.get_param('~frameId', 'radar')
 
     while not rospy.is_shutdown():
-        try:
-            radar_data = radar.getData()
-        except socket.error:
-            break
+      try:
+        radar_data = radar.getData()
+      except socket.error:
+        break
 
-        if radar_data is not None:
+      if radar_data is not None:
+        sector = RadarSectorStamped()
+        sector.header.stamp = rospy.get_rostime()
+        sector.header.frame_id = frame_id
 
-            sector = RadarSectorStamped()
-            sector.header.stamp = rospy.get_rostime()
-            sector.header.frame_id = 'radar'
-
-            for sl in radar_data['scanlines']:
-                scanline = RadarScanline()
-                scanline.angle = sl['angle']
-                scanline.range = sl['range']
-                scanline.intensities = sl['intensities']
-                sector.sector.scanlines.append(scanline)
-            pub.publish(sector)
+        for sl in radar_data['scanlines']:
+          scanline = RadarScanline()
+          scanline.angle = sl['angle']
+          scanline.range = sl['range']
+          scanline.intensities = sl['intensities']
+          sector.sector.scanlines.append(scanline)
+        pub.publish(sector)
 
 if __name__ == '__main__':
-    try:
-        radar_listener()
-    except rospy.ROSInterruptException:
-        pass
+  try:
+    radar_listener()
+  except rospy.ROSInterruptException:
+    pass
